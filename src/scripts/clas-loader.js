93ec295816a8a773bdcd2b6673aaaace2f35d5ef
Joana Devesas Carvalhal <up202306568@edu.fe.up.pt>
2026-04-11 00:18:13 +0100

    clas improvements

--- src/scripts/clas-loader.js
+++ src/scripts/clas-loader.js
@@ -19,16 +19,14 @@ function renderClas(clas) {
             <div class="clas-card-inner">
                 <!-- FRONT -->
                 <div class="clas-card-front">
-                    <h2 class="clas-label">${clan.title}</h2>
+                    <h2 class="clas-label">Clã ${clan.label} - ${clan.title}</h2>
                 </div>
                 
                 <!-- BACK -->
                 <div class="clas-card-back">
                     <div class="clas-back-content">
                         <div>
-                            <span class="clas-id-badge">#${clan.id}</span>
-                            <h3 class="clas-title">${clan.title}</h3>
-                            <p class="clas-id-info">Clã do Cenáculo Núcleo Norte</p>
+                            <h3 class="clas-title">Agrup ${clan.id} - ${clan.title}</h3>
                         </div>
                         <a href="mailto:${clan.email}" class="clas-email">
                             <i class="fa fa-envelope"></i>
