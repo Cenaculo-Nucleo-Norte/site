6edd738576510aad842e43d78524e782e5afa41f
Joana Devesas Carvalhal <up202306568@edu.fe.up.pt>
2026-04-10 09:16:40 +0100

    fixes to width

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -19,11 +19,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const navbar = document.querySelector('.navbar');
     window.addEventListener('scroll', () => {
         if (window.scrollY > 10) {
-            navbar.style.boxShadow = '0 4px 20px rgba(0, 0, 0, 0.1)';
-            navbar.style.padding = '0.4rem 1rem'; 
+            navbar.classList.add('scrolled');
         } else {
-            navbar.style.boxShadow = 'none';
-            navbar.style.padding = '0.6rem 1rem';
+            navbar.classList.remove('scrolled');
         }
     });
 });
