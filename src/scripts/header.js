20e9f146cf0a447d04caa2a1025da4d66ade67fc
Joana Devesas Carvalhal <up202306568@edu.fe.up.pt>
2026-04-15 22:26:14 +0100

    header fix

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -15,13 +15,4 @@ document.addEventListener('DOMContentLoaded', () => {
             link.classList.add('active');
         }
     });
-
-    const navbar = document.querySelector('.navbar');
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 10) {
-            navbar.classList.add('scrolled');
-        } else {
-            navbar.classList.remove('scrolled');
-        }
-    });
 });
